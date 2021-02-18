3bf95bc44068c293fe48ee0d3baf905be849a2ad
Antonin <antonin.wieczorek@efrei.net>
2021-02-18 18:13:57 +0100

    Add Comments

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -30,54 +30,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void send(View view){
+    //AUTHENTIFICATE BUTTON TRIGGER
+    public void send(View view) {
+        EditText inputName = (EditText) findViewById(R.id.inputName);
+        EditText inputPass = (EditText) findViewById(R.id.inputPass);
+
+        //Launch new thread on our authentification service
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                URL url = null;
+                String credential = "";
+                try {
+                    url = new URL(" https://httpbin.org/basic-auth/bob/sympa");
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    StringBuilder builder = new StringBuilder();
+                    builder.append(inputName.getText().toString()).append(":").append(inputPass.getText().toString());
+
+                    //Add the Authentification parameter from our two form input
+                    String basicAuth = "Basic " + Base64.encodeToString(builder.toString().getBytes(), Base64.NO_WRAP);
+                    urlConnection.setRequestProperty("Authorization", basicAuth);
+                    try {
+                        InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                        JSONObject s = readStream(in);
+                        credential = "Authentificated : " + s.getString("authenticated") + " ; " + "User : " + s.getString("user");
+                        Log.i("JFL", credential);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } finally {
+                        urlConnection.disconnect();
+                    }
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                String finalCredential = credential;
+
+                // Modify our main view with a runOnUiThread
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView name = (TextView) findViewById(R.id.credentialsLabel);
+                        if (finalCredential.length() == 0) {
+                            Log.i("cred", "Unable to connect");
+                            name.setText(new String("Unable to connect"));
+                        } else {
+                            name.setText(finalCredential);
+                        }
+                    }
+                });
+            }
+        });
         thread.start();
     }
-
-    private Thread thread = new Thread(){
-        public void run(){
-            URL url = null;
-            String credential="";
-            try {
-                url = new URL(" https://httpbin.org/basic-auth/bob/sympa");
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                EditText inputName = (EditText) findViewById(R.id.inputName);
-                EditText inputPass = (EditText) findViewById(R.id.inputPass);
-                StringBuilder builder = new StringBuilder();
-                builder.append(inputName.getText().toString()).append(":").append(inputPass.getText().toString());
-                String basicAuth = "Basic " + Base64.encodeToString(builder.toString().getBytes(), Base64.NO_WRAP);
-                urlConnection.setRequestProperty ("Authorization", basicAuth);
-                try {
-                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
-                    JSONObject s = readStream(in);
-                    credential = "Authentificated : " + s.getString("authenticated") + " ; " + "User : " + s.getString("user");
-                    Log.i("JFL", credential);
-                } catch (JSONException e){
-                 e.printStackTrace();
-                }finally {
-                    urlConnection.disconnect();
-                }
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            String finalCredential = credential;
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    TextView name = (TextView)findViewById(R.id.credentialsLabel);
-                    name.setText(finalCredential);
-                }
-            });
-        }
-    };
 
     private JSONObject readStream(InputStream is) {
         try {
             ByteArrayOutputStream bo = new ByteArrayOutputStream();
             int i = is.read();
-            while(i != -1) {
+            while (i != -1) {
                 bo.write(i);
                 i = is.read();
             }
